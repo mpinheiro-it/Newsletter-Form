9d81df6da8b4c32835e51d98d36c2469279a4e3d
Murilo <pinheiromurilo13@gmail.com>
2021-12-27 12:49:10 -0300

    adicionando conexao com DB MySQL local e instrucoes para metodo POST

---
+++ index.js
@@ -0,0 +1,62 @@
+//Express
+const express = require("express");
+const app = express();
+
+
+//MySql2
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'users',
+    password: "Hebreus12:11",
+  });
+
+
+//Serve os arquivos estaticos automaticamente
+app.use(express.static("public"));
+
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+
+
+//Subindo servidor
+app.listen(8080, () => {
+
+    console.log("servidor local rodando na porta 8080")
+
+});
+
+app.get("/users", (req, res) => {
+
+     connection.query(
+            'SELECT * FROM users',
+            function(err, results, fields) {     
+                 res.send(results); // results contains rows returned by server              
+            }
+          );    
+})
+
+app.post("/users",  (req, res) => {
+
+    const { name, sobrenome, email } = req.body;
+
+    const sql = "INSERT INTO users (name, lastName, email) VALUES (?,?,?)";
+    
+    const fields = [req.body.name, req.body.sobrenome, req.body.email]; 
+    
+   connection.query(sql, fields, function (err, results) {
+
+            if (err) throw err;
+            console.log("record inserted")
+        }
+    )
+
+    res.send("<h1>Muito obrigado! :)</h1>");
+    //res.end("__dirname + '/public/index.html'");
+
+})
+
+
+
